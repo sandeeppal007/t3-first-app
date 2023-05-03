1df03e77af27416c832f7c70ff7560c2af324f63
sandeep pal <sandeeppal7208@gmail.com>
2023-05-03 20:36:34 +0530

    auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import {ClerkProvider} from "@clerk/nextjs"
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default MyApp;
